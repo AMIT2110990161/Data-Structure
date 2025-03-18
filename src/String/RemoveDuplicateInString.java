3be1feff39d792fc99e7fc4bacde912c1ccc5fb2
AMIT KUMAR <amit70kumar14@gmail.com>
2025-03-18 14:19:00 +0530

    [Amit kumar] Add, Remove Duplicates from a String Using StringBuilder

---
+++ src/String/RemoveDuplicateInString.java
@@ -0,0 +1,18 @@
+package String;
+import java.util.*;
+
+public class RemoveDuplicateInString {
+    public static void main(String[] args) {
+        String str = "ananassmjacsb";
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> hs = new HashSet<>();
+
+        for(char ch:str.toCharArray()){
+            if(!hs.contains(ch)){
+                sb.append(ch);
+                hs.add(ch);
+            }
+        }
+        System.out.println(sb);
+    }
+}
