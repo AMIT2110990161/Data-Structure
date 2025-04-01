9dbc92ef0e1c65516a8ea67150270802785992c6
AMIT KUMAR <amit70kumar14@gmail.com>
2025-04-01 15:11:26 +0530

    m

--- src/SortingAlgorithem/BubbleSort.java
+++ src/SortingAlgorithem/BubbleSort.java
@@ -1,29 +1,39 @@
 package SortingAlgorithem;
-import java.util.*;
 
 public class BubbleSort {
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        boolean swapped;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+    public static void main(String[] args) {
+        int[] studentMarks = {5,4,3,2,1};
+        sort(studentMarks);
+        printMarks(studentMarks);
+    }
+
+    public static void sort(int[] arr){
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++){
+            boolean check = false;
+            for(int j=1; j<arr.length-i; j++){
+                if(arr[j-1] > arr[j]){
+//                    int temp = arr[j];
+//                    arr[j] = arr[j-1];
+//                    arr[j-1] = temp;
+                    swap(arr, j-1, j);
+                    check = true;
+                    cnt++;
                 }
             }
-            if (!swapped) break; // Optimization: Stop if no swaps occurred
+            cnt++;
+            if(!check)break;
+        }
+        System.out.println(cnt);
+    }
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    public static void printMarks(int[] arr){
+        for(int a:arr){
+            System.out.print(a + " ");
         }
     }
-
-    public static void main(String[] args) {
-        int[] arr = {5, 3, 8, 4, 2};
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
 }
-
